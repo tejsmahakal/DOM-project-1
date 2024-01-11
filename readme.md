28d122d07f2527b1eb354731874afcee45739c0b
tejas mahakal <tejasmahakal740@gmail.com>
2024-01-11 11:44:30 +0530

    DOM project 1

---
+++ readme.md
@@ -0,0 +1,1 @@
+DOM project 1
---
+++ script.js
@@ -0,0 +1,9 @@
+const targetDiv = document.getElementById("targetDiv");
+   targetDiv.style.backgroundColor = "yellow";
+   targetDiv.style.color= "blue";
+   targetDiv.style.margin = "30px";
+   targetDiv.style.padding = "20px";
+   targetDiv.style.fontSize = "30px";
+   targetDiv.style.fontWeight = "bold";
+   targetDiv.style.height = "300px";
+   targetDiv.style.width = "450px";
